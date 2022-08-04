bfb69e1088a3f51c36116de5ce30e9330a582e34
Sweta-Das <dassweta1997@gmail.com>
2022-08-05 01:02:06 +0545

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first app'),
-      centerTitle: true,
-      backgroundColor: Colors.redAccent,
-    ),
-  body: Center(
-    child: Text('Hello World!',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.grey[600],
-                ),),
-  ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('Click'),
-      backgroundColor: Colors.redAccent,
-    ),
-  ),
+  home: Home(),
 ));
+
+//Creating a stateless widget (state that can't change over time) called test
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  //build function is responsible for building a widget tree within stateless
+  //Home widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Text('Hello World!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Click Me!!'),
+        backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
