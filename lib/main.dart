b2ee613d7f40b7248cc408b705cc7c939c864e87
Sweta-Das <dassweta1997@gmail.com>
2022-08-06 22:32:28 +0545

    Icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
-//Creating a stateless widget (state that can't change over time) called test
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  //build function is responsible for building a widget tree within stateless
-  //Home widget
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
         title: Text('My first app'),
         centerTitle: true,
         backgroundColor: Colors.grey[600],
       ),
+
       body: Center(
-        child: Image.asset('Assets/wallpaper.jpeg')
-            // Also Image.network('https://mobimg.b-cdn.net/v3/fetch/05/05eeb93a2e41734ecb6044146351f11e.jpeg')
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+        ),
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Text('Click'),
         backgroundColor: Colors.grey[600],
       ),
